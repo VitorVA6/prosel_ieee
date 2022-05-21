3f0181c5a536066993fe1e8403d79af987413b60
Vitor Vaz Andrade <vitorandrade@ecomp.uefs.br>
2022-05-21 19:00:19 -0300

    update

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 
+#criando o dataser
 dados = pd.read_csv('https://gist.githubusercontent.com/guilhermesilveira/1b7d5475863c15f484ac495bd70975cf/raw/16aff7a0aee67e7c100a2a48b676a2d2d142f646/projects.csv')
+#convertendo de pandas pra um array numpy
 dados = dados.to_numpy()
 
+#função que sepata os dados em 2 arrays, um para trabalhos finalizados e outro para não finalizados e em seguida os plota em cores diferentes
 def plot_data(dados_x, dados_y):
     arr_filter = []
     for elem in dados_y:
@@ -29,12 +31,16 @@ def plot_data(dados_x, dados_y):
 x = dados[0:, 1:]
 y = dados[0:, 0]
 
-
-x_train, x_test, y_train, y_test = train_test_split(x, y)
+#definindo tamanho do conjunto de teste
+test_size = 0.2
+#separando os conjuntos de treino e de teste
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
 
 plot_data(x_test, y_test)
 
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(30, 2), random_state=1, max_iter=20000)
+#Criando o classificador
+clf = DecisionTreeClassifier()
+#Treinando o classificador com o conjunto de treino
 clf.fit(x_train, y_train)
-
+#Testando e medindo acurácia com o conjunto de teste
 print(clf.score(x_test, y_test))
